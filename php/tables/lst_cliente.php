2c5bc900ee5a189212eedf4c8d198be6742db2c8
Charles Borges <solucao.erp@gmail.com>
2022-11-04 18:13:28 -0300

    table: estrutura modelo

---
+++ php/tables/lst_cliente.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista Cliente</title>
+</head>
+
+<body>
+    <h1>Listagem de Clientes</h1>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>E-mail</th>
+                <th>Editar</th>
+                <th>Excluir</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Charles Borges</td>
+                <td>charles.borges@gmail.com</td>
+                <td><a href="#">Editar</a></td>
+                <td><a href="#">Excluir</a></td>
+            </tr>
+            <tr>
+                <td>Robson Rodrigues</td>
+                <td>robson.rodrigues@gmail.com</td>
+                <td><a href="#">Editar</a></td>
+                <td><a href="#">Excluir</a></td>
+            </tr>
+            <tr>
+                <td>Melissa Costa e Silva</td>
+                <td>melissa.silva@gmail.com</td>
+                <td><a href="#">Editar</a></td>
+                <td><a href="#">Excluir</a></td>
+            </tr>
+            <tr>
+                <td>Castro Alvares</td>
+                <td>castro.alvares@gmail.com</td>
+                <td><a href="#">Editar</a></td>
+                <td><a href="#">Excluir</a></td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+
+</html>
---
+++ php/tables/lst_produto.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista Produto</title>
+</head>
+
+<body>
+    <h1>Listagem de Produtos</h1>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Produto</th>
+                <th>Preço</th>
+                <th>Quantidade</th>
+                <th>Subtotal</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Mouse</td>
+                <td>R$ 50,00</td>
+                <td>2,00</td>
+                <td>R$ 100,00</td>
+            </tr>
+            <tr>
+                <td>Kit Teclado</td>
+                <td>R$ 125,40</td>
+                <td>3,00</td>
+                <td>R$ 376,20</td>
+            </tr>
+            <tr>
+                <td>Gabinete Torre</td>
+                <td>R$ 210,00</td>
+                <td>2,00</td>
+                <td>R$ 420,00</td>
+            </tr>
+        </tbody>
+        <tfoot>
+            <tr>
+                <td colspan="3">Total</td>
+                <td>R$ 896,20</td>
+            </tr>
+        </tfoot>
+    </table>
+</body>
+
+</html>
